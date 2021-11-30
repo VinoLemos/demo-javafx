23fe5c865d09edc406ee62beb61eb3922aaba051
Vinicius Lemos <vlemosdeoliveira@gmail.com>
2021-11-30 17:57:48 -0300

    comboBox

---
+++ src/gui/ViewController.java
@@ -0,0 +1,76 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import model.entities.Person;
+
+public class ViewController implements Initializable {
+
+	@FXML
+	private ComboBox<Person> comboBoxPerson;
+	
+	@FXML
+	private Button btAll;
+
+	@FXML
+	public void onBtAllAction() {
+		for (Person person : comboBoxPerson.getItems()) {
+			System.out.println(person);
+		}
+			
+	}	
+	
+	// Método da Action a ser implementada no ComboBox
+	@FXML
+	public void onComboBoxPersonAction() {
+		// Seleciona o item escolhido no ComboBox
+		Person person = comboBoxPerson.getSelectionModel().getSelectedItem();
+		System.out.println(person);
+	}		
+	
+	
+	// Tipo especial de lista que associa a coleção de objetos com o componente
+	// visual
+	private ObservableList<Person> obsList;
+
+
+	// Método responsável por inicializar os métodos de limitações na instanciação
+	// do controlador
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		List<Person> list = new ArrayList<>();
+		list.add(new Person(1, "Maria", "maria@gmail.com"));
+		list.add(new Person(2, "Alex", "alex@gmail.com"));
+		list.add(new Person(3, "Bob", "bob@gmail.com"));
+
+		// Associa a coleção de objetos à lista especial obsList
+		obsList = FXCollections.observableArrayList(list);
+
+		// Carrega os elementos da lista especial no componente visual
+		comboBoxPerson.setItems(obsList);
+
+		// Método que mostrará apenas o nome da pessoa dentro do ComboBox
+		Callback<ListView<Person>, ListCell<Person>> factory = lv -> new ListCell<Person>() {
+			@Override
+			protected void updateItem(Person item, boolean empty) {
+				super.updateItem(item, empty);
+				setText(empty ? "" : item.getName());
+			}
+		};
+		comboBoxPerson.setCellFactory(factory);
+		comboBoxPerson.setButtonCell(factory.call(null));
+		
+	}
+}
